0f445ae322b084b8237641007194194e30f3b5b5
Christopher Mrakovcic <cmrakovcic@aol.com>
2020-12-15 14:50:15 -0500

    commit tree bdc72e8fd97b86625b730a946726a229b001583c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def add_student(student, grade)
+    if @roster[grade]
+        @roster[grade] << student
+    else @roster[grade] = [student]
+    end
+end
+def grade(grade)
+    @roster[grade]
+end
+def sort
+    hash = {}
+    @roster.sort.each do |grade, student_array|
+        hash[grade] = (student_array.sort)
+    end
+    hash
+end
+end
